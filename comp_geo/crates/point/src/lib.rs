e7c4262e97f5bea7fa435bc46336574d383761a7
Mark Meyer <mark.meyer@gsa.gov>
2025-12-23 11:21:14 -0500

    fix point xproduct. Add segment with intersection

--- comp_geo/crates/point/src/lib.rs
+++ comp_geo/crates/point/src/lib.rs
@@ -1,5 +1,6 @@
 use num_traits::Num;
 use std::fmt::Debug;
+use std::ops::{Add, Sub};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Point<const D: usize, N> {
@@ -28,6 +29,13 @@ impl<N: Copy> Point<2, N> {
     pub fn y(&self) -> N {
         self.coords[1]
     }
+
+    pub fn cross(&self, other:&Self) -> N
+    where 
+        N: Num
+    {
+        self.x() * other.y() - self.y() * other.x()
+    }
 }
 
 // 3D implementations
@@ -43,7 +51,13 @@ impl<N: Copy> Point<3, N> {
     }
 }
 
-// Generic math functions
+/// Square Distance between two points
+/// ```
+/// use point::Point;
+/// let p1 = Point {coords: [3, 4]};
+/// let p2 = Point {coords: [0, 0]};
+/// assert_eq!(&p1.square_distance(&p2), &25)
+/// ```
 impl<const D: usize, N: Num + Copy> Point<D, N> {
     pub fn square_distance(&self, other: &Self) -> N
     where
@@ -54,6 +68,61 @@ impl<const D: usize, N: Num + Copy> Point<D, N> {
             .zip(other.coords.iter())
             .map(|(&a, &b)| (a - b) * (a - b))
             .sum()
+    }
+}
+/// Addition betweein points
+/// ```
+/// use point::Point;
+/// let p1 = Point {coords: [3, 4]};
+/// let p2 = Point {coords: [1, -1]};
+/// assert_eq!(p1 + p2, Point {coords: [4, 3]})
+/// ```
+impl<const D: usize, N: Num + Copy> Add for Point<D, N> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let coords = std::array::from_fn(|i| {
+            self.coords[i] + rhs.coords[i]
+        });
+
+        Point { coords }
+    }
+}
+
+/// ```
+/// use point::Point;
+/// let p1 = Point {coords: [3, 4]};
+/// let p2 = Point {coords: [1, -1]};
+/// assert_eq!(&p1 + &p2, Point {coords: [4, 3]})
+/// ```
+impl<'a, 'b, const D: usize, N: Num + Copy> Add<&'b Point<D, N>> for &'a Point<D, N> {
+    type Output = Point<D, N>;
+
+    fn add(self, rhs: &'b Point<D, N>) -> Self::Output {
+        Point {
+            coords: std::array::from_fn(|i| self.coords[i] + rhs.coords[i])
+        }
+    }
+}
+
+impl<const D: usize, N: Num + Copy> Sub for Point<D, N> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let coords = std::array::from_fn(|i| {
+            self.coords[i] - rhs.coords[i]
+        });
+        Point { coords }
+    }
+}
+
+impl<'a, 'b, const D: usize, N: Num + Copy> Sub<&'b Point<D, N>> for &'a Point<D, N> {
+    type Output = Point<D, N>;
+
+    fn sub(self, rhs: &'b Point<D, N>) -> Self::Output {
+        Point {
+            coords: std::array::from_fn(|i| self.coords[i] - rhs.coords[i])
+        }
     }
 }
 
